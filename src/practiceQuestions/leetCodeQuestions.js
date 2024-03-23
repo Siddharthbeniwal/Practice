4ae3418b8629a1637ebc9c9c9844c5ef7f994d7f
Siddharth Beniwal <siddharthben16@gmail.com>
2024-03-23 21:17:25 +0530

    leetcode-3sum

--- src/practiceQuestions/leetCodeQuestions.js
+++ src/practiceQuestions/leetCodeQuestions.js
@@ -80,6 +80,81 @@ export default function LeetCodeQuestions() {
 // console.log("maxLength", maxLength);
 
 
+// ***********************************************************************************************************************************************************
+
+// Q.3
+// https://leetcode.com/problems/3sum/description/
+
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and 
+// nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
 
 
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation: 
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+
+// Example 2:
+
+// Input: nums = [0,1,1]
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+
+// Example 3:
+
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+
+
+// Constraints:
+// 3 <= nums.length <= 3000
+// -105 <= nums[i] <= 105
+
+// const nums = [-1, 0, 1, 2, -1, -4];
+
+// function threeSum(nums) {
+//   nums.sort((a, b) => a - b);
+//   let resultArr = [];
+
+//   for (let i = 0; i < nums.length - 2; i++) {
+
+//     let start = i + 1;
+//     let end = nums.length - 1;
+
+//     function checkDuplicate(currArr) {
+//       for (const triplet of resultArr) {
+//         if (triplet.every((value, i) => value === currArr[i])) return true;
+//       }
+//       return false;
+//     }
+
+//     while (start < end) {
+//       let currSum = nums[i] + nums[start] + nums[end];
+//       let currArr = [nums[i], nums[start], nums[end]];
+
+//       if (currSum === 0) {
+//         let isDuplicate = checkDuplicate(currArr);
+//         if (!isDuplicate) {
+//           resultArr.push(currArr);
+//         }
+//         start++;
+//       } else if (currSum < 0) {
+//         start++;
+//       } else {
+//         end--;
+//       }
+//     }
+//   }
+//   return resultArr;
+// }
+
+// console.log(threeSum(nums));
 
